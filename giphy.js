b39b30cd40629c8e98b8938fac7abbed06f07bc3
Luke Lemke <llemke777@gmail.com>
2023-11-13 17:01:32 -0600

    second commit

--- giphy.js
+++ giphy.js
@@ -1,6 +1,30 @@
 console.log("Let's get this party started!");
 
-async function getGif(){
-    const response = await axios.get("api.giphy.com/v1/gifs/trending");
-    console.log(response);
+async function getGif(searchTerm) {
+    try {
+        const response = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+        
+        
+        const gifUrl = response.data.data[0].images.original.url;
+
+        
+        const img = document.createElement('img');
+        img.src = gifUrl;
+
+        
+        document.body.appendChild(img);
+    } catch (error) {
+        console.error('Error fetching GIF:', error);
+    }
 }
+
+document.getElementById('searchForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const inputValue = document.getElementById('search').value;
+    await getGif(inputValue);
+});
+
+document.getElementById('removeGif').addEventListener('click', function () {
+    
+    document.querySelectorAll('img').forEach(img => img.remove());
+});
